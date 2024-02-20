c3e642e99c97fc0e718c06e893c4bb51a789a73e
yairzawi770 <148704993+yairzawi770@users.noreply.github.com>
2024-02-20 20:30:02 +0200

    1

--- js/16.  28.1.24 JQUERY/index1.js
+++ js/16.  28.1.24 JQUERY/index1.js
@@ -1,32 +1,36 @@
 function onclick() {
-    
-}
 
+}
+//1 options
 //window.onload = ...
+
+//2 options
 // $(document).ready(() => { })
+
+//3 options
 $(() => {
     //document.getElementById('btn1').addEventListener('click', () => {alert('clicked')})
     //document.getElementById('btn1') = $('#btn1')
     //$('#btn1').addEventListener('click', () => {alert('clicked')})
     $('#btn1').on('click', () => {
-        $('#my_h1').css( {color: 'green', 'font-weight': 'bold'} )
+        $('#my_h1').css({ color: 'green', 'font-weight': 'bold' })
     })
 
     $('#btn1').on(
-        {
-        'click': () => {
-            $('#my_h1').css( {color: 'green', 'font-weight': 'bold'} )
-        },
-        'mouseenter': () => {
-            $('#my_h1').css( {color: 'blue', 'font-weight': 'bold'} )
-        }
-    })
+        {  
+            'click': () => {
+                $('#my_h1').css({ color: 'green', 'font-weight': 'bold' })
+            },
+            'mouseenter': () => {
+                $('#my_h1').css({ color: 'blue', 'font-weight': 'bold' })
+            }
+        })
 
     $('#btn2').on('click', () => {
-        $('#my_h1').css( {color: 'black', 'font-weight': ''} )
+        $('#my_h1').css({ color: 'black', 'font-weight': '' })
         $('#btn1').off() // remove all events -- click, onmouseenter
     })
-  
+
     // function click_me() { }
     // document.getElementById('btn1').addEventListener('click',click_me)
     // document.getElementById('btn1').removeEventListener('click', click_me)
@@ -34,7 +38,7 @@ $(() => {
     //$('#btn1').off('click') // remove event click
     //$('#btn1').off() // remove all
 
-    $('#my_h1').css( {color: 'red', 'font-weight': 'bold'} )
+    $('#my_h1').css({ color: 'red', 'font-weight': 'bold' })
 
     // create a form with 4 buttons
     // create a <p>Hello JQuery</p>
